39de8cdbd32c39eaa53a18d0f1ec76d16f954ca9
Umair Abdus Salam <umairabdsalam@gmail.com>
2024-06-12 23:51:20 +0100

    Initial Commit

---
+++ ZHR_SuccessFactors/ZHR_SuccessFactorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZHR_SuccessFactorsApp.swift
+//  ZHR_SuccessFactors
+//
+//  Created by Umair Salam on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ZHR_SuccessFactorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
